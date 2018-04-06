95e273dbe1c1b6e4fed2169fe52debf7977a656a
Eric Sharkey <ers16e@my.fsu.edu>
2018-04-06 11:10:58 -0400

    Fixed Smoothing Function

--- LoadData.m
+++ LoadData.m
@@ -4,6 +4,11 @@ FileList = dir('*.mat');
 name = {FileList.name}.';
 maxSamples = 5207;
 
+%If the number of snippets isn't defined, use the whole dataset
+if exist('numsnippets','var') == 0
+    numsnippets = maxSamples;
+end
+
 %Creates an index for the snippets for any number of snippets selected
 bound = length(name) - numsnippets + 1;
 subset = randi(bound,1,1);
@@ -41,11 +46,4 @@ data = data';
 totallength = size(data,1) * size(data,2);
 
 waves = reshape(data, [totallength, 1]);
-sigma = 20;
-sz = 30;    % length of gaussFilter vector
-x = linspace(-sz / 2, sz / 2, sz);
-gaussFilter = exp(-x .^ 2 / (2 * sigma ^ 2));
-gaussFilter = gaussFilter / sum (gaussFilter); % normalize
-filtereddata = filter(gaussFilter,1,waves);
 
-data = reshape(waves, [10000, numsnippets]);
--- transformdata.m
+++ transformdata.m
@@ -8,10 +8,13 @@ Fs = 1000;
 
 [pxx,f] = periodogram(dataset,hann(length(dataset)),length(dataset),Fs);
 
-% smoothed = zeros(size(pxx));
-% for i = 1:numsnippets
-%     smoothed(:,i) = smooth(pxx(:,i));
-% end
+sigma = 20;
+sz = 30;    % length of gaussFilter vector
+x = linspace(-sz / 2, sz / 2, sz);
+gaussFilter = exp(-x .^ 2 / (2 * sigma ^ 2));
+gaussFilter = gaussFilter / sum (gaussFilter);
+pxx = filter(gaussFilter,1,pxx,[],2);
+
 
 % pxx = smoothed;
 
