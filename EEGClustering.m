7036c9877cd01ea9695d2744f2f1327d456c25a7
Eric Sharkey <ers16e@my.fsu.edu>
2018-09-13 16:47:12 +0000

    Created EEGClustering Function

---
+++ EEGClustering.m
@@ -0,0 +1,14 @@
+
+%% Identify Clusters in EEG signals using Spectral Clustering
+
+% Input:
+%   data: EEG Signals in the form (M x N), where M is the number of samples in each signal, and N is the number of signals.
+%   temporal: Indicates whether or not the signals are in temporal order,
+%   which can be used to improve clustering results.
+
+% Output:
+    
+function [idx] = EEGClustering(data, temporal)
+    
+    
+end
